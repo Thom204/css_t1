b1c06d85dbd7d39b3bd8fdc5b16bbc523ad8d2dc
Thom204 <tmonnier@unal.edu.co>
2026-01-18 14:38:21 -0500

    thingies

--- scripts/expenseTracker.js
+++ scripts/expenseTracker.js
@@ -1,7 +1,46 @@
 const SUPABASE_URL = 'https://kcgafrfckiaqsvvhgrzf.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_7uE2TT6IsgrXOPFpYWisyg_nmcyDoL7'
 
+const existent_plan = document.querySelector("#mydashboard")
+const new_plan = document.querySelector("#mydashboard")
+const config = document.querySelector('#settings')
+const logout_b = document.getElementById('logout_btn')
+
 const sb = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 )
+
+existent_plan.onclick = () => {
+    window.location.href = "viewer.hmtl"
+}
+
+new_plan.onclick = () => {
+    window.location.href = "create.html"
+}
+
+logout_b.onclick = async () => {
+    console.log("szs")
+    await sb.auth.signOut()
+}
+
+async function  updateUI() {
+    const { data: { user } } = await sb.auth.getUser()
+
+    if (user) {
+        document.querySelector("#username").innerHTML = user.email;
+        console.log(user.email)
+    } else {
+        console.log("nanay")
+        window.location.href = "index.html"
+    }
+}
+
+// Listen for auth changes
+sb.auth.onAuthStateChange(() => {
+    updateUI()
+    console.log(event, session?.user?.email)
+})
+
+// Initial check
+updateUI()
--- scripts/main.js
+++ scripts/main.js
@@ -2,43 +2,45 @@ const SUPABASE_URL = 'https://kcgafrfckiaqsvvhgrzf.supabase.co'
 const SUPABASE_ANON_KEY = 'sb_publishable_7uE2TT6IsgrXOPFpYWisyg_nmcyDoL7'
 
 const sb = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
 )
 
 const loginBtn = document.getElementById('login')
-const logoutBtn = document.getElementById('logout')
+const inviteBtn = document.getElementById('invite')
 const userText = document.getElementById('user')
+
 
 // Login
 loginBtn.onclick = async () => {
-  await sb.auth.signInWithOAuth({
-    provider: 'google',
-  })
+    await sb.auth.signInWithOAuth({
+        provider: 'google',
+    })
 }
 
 // Logout
-logoutBtn.onclick = async () => {
-  await sb.auth.signOut()
+inviteBtn.onclick = async () => {
+    return
 }
 
 // Update UI based on auth state
 async function updateUI() {
-  const { data: { user } } = await sb.auth.getUser()
+    const { data: { user } } = await sb.auth.getUser()
 
-  if (user) {
-    window.location.href = "neighborhood.html"
-  } else {
-    userText.textContent = ''
-    loginBtn.style.display = 'inline'
-    logoutBtn.style.display = 'none'
-  }
+    if (user) {
+        console.log(user)
+        window.location.href = "app.html"
+    } else {
+        userText.textContent = ''
+        loginBtn.style.display = 'inline'
+        logoutBtn.style.display = 'none'
+    }
 }
 
 // Listen for auth changes
 sb.auth.onAuthStateChange(() => {
-  updateUI()
-  console.log(event, session?.user?.email)
+    updateUI()
+    console.log(event, session?.user?.email)
 })
 
 // Initial check
